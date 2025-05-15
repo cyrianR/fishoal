858c7363d65365440ce6bb193e7f01ec65317da8
cyrianR <ragotcyrian@protonmail.com>
2025-05-15 11:34:30 +0200

    jupyter notebook added

--- src/aquarium.py
+++ src/aquarium.py
@@ -38,25 +38,3 @@ class AquariumKDTree2D(Aquarium2D):
     def update_all(self):
         self.kdtree = sp.spatial.KDTree([fish.position for fish in self.fishes])
         super().update_all()
-
-
-
-class RandomPositionsAquarium2D(Aquarium2D):
-    def __init__(self, width: int, height: int, n_fishes: int, color: str, behavior: "Behavior", dt: float):
-        super().__init__(width, height, n_fishes, dt)
-        self.populate_fishes(n_fishes, color, behavior, width, height)
-
-    def populate_fishes(self, n_fishes: int, color: str, behavior: "Behavior", width: int, height: int):
-        for i in range(n_fishes):
-            position = np.random.rand(2) * [width, height]
-            velocity = (np.random.rand(2) * 2 - 1)
-            velocity /= np.linalg.norm(velocity)
-            self.fishes[i] = Fish(position, velocity, color, behavior)
-
-    def change_fish_colors(self, new_color: str):
-        for fish in self.fishes:
-            fish.color = new_color
-
-    def change_fish_behaviors(self, new_behavior: "Behavior"):
-        for fish in self.fishes:
-            fish.behavior = new_behavior
